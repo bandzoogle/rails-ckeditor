6bb1e75769a4edf5e147e30eed0bad8f78f123b1
Colin Mitchell <muffinista@gmail.com>
2013-03-19 16:23:51 -0400

    dont package _source

--- lib/ckeditor-rails/engine.rb
+++ lib/ckeditor-rails/engine.rb
@@ -2,7 +2,7 @@ module Ckeditor
   module Rails
     class Engine < ::Rails::Engine
       initializer 'configure assets of ckeditor', :group => :all do |app|
-        app.config.assets.precompile += %w( ckeditor/*.js ckeditor/*.css ckeditor/*.png )
+        app.config.assets.precompile -= %w( ckeditor/_source )
       end
     end
   end
--- lib/ckeditor-rails/railtie.rb
+++ lib/ckeditor-rails/railtie.rb
@@ -3,6 +3,7 @@ module Ckeditor
     class Railtie < ::Rails::Railtie
       initializer 'configure assets of ckeditor', :group => :all do |app|
         app.config.assets.precompile += %w( ckeditor/*.js ckeditor/*.css ckeditor/*.png )
+        app.config.assets.precompile -= %w( ckeditor/_source )
       end
     end
   end
